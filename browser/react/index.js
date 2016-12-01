1d3522dd31756e7023f257067cee05a582543ed5
Garity Chapman <garitychapman@gmail.com>
2016-12-01 18:30:12 -0500

    set up cloneElement and IndexRedirect

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory,  IndexRedirect } from 'react-router';
+import Albums from './components/Albums';
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={AppContainer}>
-
+    	<IndexRedirect to="/albums" />
+    	<Route path='albums' component={Albums}/>
     </Route>
   </Router>,
   document.getElementById('app')
